e87046dcba7f05624722e25c4fc31b9a9c025c83
So-Gau <sophie.gaubert10@gmail.com>
2022-02-03 21:45:26 +0100

    add repositoryProject for homecontroller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\SkillRepository;
+use App\Repository\ProjectRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +13,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-   public function index(SkillRepository $skillRepository): Response
+   public function index(SkillRepository $skillRepository, ProjectRespository $projectRespository): Response
    {
        return $this->render('home/index.html.twig', [
             'skills' => $skillRepository->findAll(),
+            'projects' => $projectRepository->findAll(),
        ]);
    }
 
